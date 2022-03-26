629b7dc18d98cdc9f25a9ffe71c146e8ba7d2431
muammarahlnn <21muammarahlan@gmail.com>
2022-03-26 22:11:01 +0800

    add update operation

--- src/handler.js
+++ src/handler.js
@@ -45,7 +45,7 @@ const addBookHandler = (request, h) => {
         });
         response.code(201);
         return response;
-    } else if (name === null) {
+    } else if (name === undefined) {
         const response = h.response({
             status: "fail",
             message: "Gagal menambahkan buku. Mohon isi nama buku",
@@ -98,8 +98,70 @@ const getBookByIdHandler = (request, h) => {
     return response;
 }
 
+const editBookByIdHandler = (request, h) => {
+    const { id } = request.params;
+    const {
+        name,
+        year,
+        author,
+        summary,
+        publisher,
+        pageCount,
+        readPage,
+        reading
+    } = request.payload;
+    const updatedAt = new Date().toISOString();
+    const finished = pageCount === readPage
+
+    const index = books.findIndex((book) => book.id === id);
+    if (index !== -1) {
+        books[index] = {
+            ...books[index],
+            name,
+            year,
+            author,
+            summary,
+            publisher,
+            pageCount,
+            readPage,
+            finished,
+            reading,
+            updatedAt,
+        };
+
+        const response = h.response({
+            status: "success",
+            message: "Buku berhasil diperbarui",
+        });
+        response.code(200);
+        return response;
+    } else if (name === undefined) {
+        const response = h.response({
+            status: "fail",
+            message: "Gagal memperbarui buku. Mohon isi nama buku",
+        });
+        response.code(400);
+        return response;
+    } else if (readPage > pageCount) {
+        const response = h.response({
+            status: "fail",
+            message: "Gagal memperbarui buku. readPage tidak boleh lebih besar dari pageCount",
+        });
+        response.code(400);
+        return response;
+    } else {
+        const response = h.response({
+            status: "fail",
+            message: "Gagal memperbarui buku. Id tidak ditemukan"
+        });
+        response.code(404);
+        return response;
+    }
+}
+
 module.exports = {
     addBookHandler,
     getAllBooksHandler,
-    getBookByIdHandler
+    getBookByIdHandler,
+    editBookByIdHandler
 };
